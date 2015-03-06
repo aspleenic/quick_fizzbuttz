6ed077115ff01a69d1e2b732e369f162065940b3
PJ Hagerty <pjhagerty@gmail.com>
2015-03-06 09:59:56 -0500

    cleanup

--- fizzbuttz.rb
+++ fizzbuttz.rb
@@ -1,16 +1,15 @@
-def fizzbuttz
-	(1..100).each do |x|
-		if x % 3 == 0 && x % 5 != 0
-			puts 'fizz'
-		elsif x % 5 == 0 && x % 3 != 0
-			puts 'buttz'
-		elsif x % 5 == 0 && x % 3 == 0
-			puts 'fizzbuttz'
-		else 
-			puts x
-		end
+(1..100).each do |x|
+	if x % 3 == 0 && x % 5 != 0
+		puts 'fizz'
+	elsif x % 5 == 0 && x % 3 != 0
+		puts 'buttz'
+	elsif x % 5 == 0 && x % 3 == 0
+		puts 'fizzbuttz'
+	else 
+		puts x
 	end
 end
+
 
 
 
